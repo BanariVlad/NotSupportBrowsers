c6cecc90f5c85ce552bff4eeeef81ea2a9dd0703
BanariVlad <banari.vlad@yandex.com>
2020-07-13 11:20:08 +0300

    Changed

--- public/browser-pass.js
+++ public/browser-pass.js
@@ -1,25 +1,64 @@
+var browsersList = [
+  {
+    browser: "Chrome",
+    minVersion: 4
+  },
+  {
+    browser: "Firefox",
+    minVersion: 2
+  },
+  {
+    browser: "Opera",
+    minVersion: 10
+  },
+  {
+    browser: "Edge",
+    minVersion: 12
+  },
+  {
+    browser: "MSIE",
+    minVersion: 9
+  },
+  {
+    browser: "IE",
+    minVersion: 11
+  },
+  {
+    browser: "Safari",
+    minVersion: 5
+  }
+];
+
 function checkBrowser() {
   var userAgent = navigator.userAgent;
   var version;
-  var browser = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+  var browser =
+    userAgent.match(
+      /(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i
+    ) || [];
 
   if (/trident/i.test(browser[1])) {
     version = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
-    return 'IE ' + (version[1] || '');
+    return "IE " + (version[1] || "");
   }
 
-  if (browser[1] === 'Chrome') {
+  if (browser[1] === "Chrome") {
     version = userAgent.match(/\b(OPR|Edge)\/(\d+)/);
     if (version !== null) {
-      return version.slice(1).join(' ').replace('OPR', 'Opera');
+      return version
+        .slice(1)
+        .join(" ")
+        .replace("OPR", "Opera");
     }
   }
 
-  browser = browser[2] ? [browser[1], browser[2]] : [navigator.appName, navigator.appVersion];
+  browser = browser[2]
+    ? [browser[1], browser[2]]
+    : [navigator.appName, navigator.appVersion];
   if ((version = userAgent.match(/version\/(\d+)/i)) !== null) {
     browser.splice(1, 1, version[1]);
   }
-  return browser.join(' ');
+  return browser.join(" ");
 }
 
 function compareVersions(currentBrowser) {
@@ -28,7 +67,10 @@ function compareVersions(currentBrowser) {
   var notSupported;
 
   for (var i = 0; i < browsersList.length; i++) {
-    if (browsersList[i].browser === browser && browsersList[i].minVersion > version) {
+    if (
+      browsersList[i].browser === browser &&
+      browsersList[i].minVersion > version
+    ) {
       notSupported = true;
     }
   }
